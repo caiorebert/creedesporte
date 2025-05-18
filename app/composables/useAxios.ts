6a4bc68f9818f5d333560df32d673ae222e360fd
Caio Rebert <caiorebert070802.cr@gmail.com>
2025-05-18 19:36:28 -0300

    implementando variável de ambiente

--- app/composables/useAxios.ts
+++ app/composables/useAxios.ts
@@ -1,7 +1,7 @@
 export const useAxios = () => {
     const { $axios } = useNuxtApp()
     //@ts-ignore
-    $axios.defaults.baseURL = process.env.API_URL || 'http://localhost:3001'
+    $axios.defaults.baseURL = process.env.NUXT_PUBLIC_API_URL || 'http://localhost:3001'
     return $axios
 }
   
